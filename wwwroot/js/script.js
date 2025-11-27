fc0a91e3a0ee0ef84f4bfc7a2075226f8582019c
rafahahon <rafaellapintohahon@gmail.com>
2025-11-27 07:56:09 -0300

    :books: Att

--- wwwroot/js/script.js
+++ wwwroot/js/script.js
@@ -1,11 +1,1 @@
-﻿const form = document.querySelector("form");
-const usuario = document.getElementById("usuario");
-const senha = document.getElementById("senha");
-const erro = document.querySelector(".erro");
-
-form.addEventListener("submit", e => {
-    if (usuario.value.trim() === "" || senha.value.trim() === "") {
-        e.preventDefault();
-        erro.textContent = "Preencha usuário e senha.";
-    }
-});
+﻿
